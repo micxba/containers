7d77675f418d3e0c43291b18dc01523d5f0d84f0
Michael Baumann <mbaumann@crunchtime.com>
2025-06-11 17:29:16 -0400

    debug

--- pg-dump/backup.sh
+++ pg-dump/backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
+
+export AWS_ACCESS_KEY_ID="$S3_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$S3_SECRET_ACCESS_KEY"
+export BACKUP_FILE=/tmp/db_backup.sql.gz
 
 echo "Starting pg_dump..."
-export BACKUP_FILE=/tmp/${POSTGRES_DATABASE}_backup.sql.gz
 PGPASSWORD="$POSTGRES_PASSWORD" pg_dump -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE" | gzip > "$BACKUP_FILE"
-echo "DEUBUG ----"
-env
-echo "DEBUG END ----"
+
 echo "Uploading to S3..."
 aws --endpoint-url "http://$S3_ENDPOINT" s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET/$S3_PREFIX/db_backup_$(date +%s).sql.gz"
