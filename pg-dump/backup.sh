5edd2954215c6f2f5415c7ff0920baa9993c96d2
Michael Baumann <mbaumann@crunchtime.com>
2025-06-11 16:05:22 -0400

    Updated pg-dump

---
+++ pg-dump/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Starting pg_dump..."
+export BACKUP_FILE=/tmp/db_backup.sql.gz
+PGPASSWORD="$POSTGRES_PASSWORD" pg_dump -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DATABASE" | gzip > "$BACKUP_FILE"
+
+echo "Uploading to S3..."
+aws --endpoint-url "http://$S3_ENDPOINT" s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET/$S3_PREFIX/db_backup_$(date +%s).sql.gz"
